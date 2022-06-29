111b47e16cf9b3b0332f92aec27769ba65a71672
RaydomeNT <raydome85@gmail.com>
2022-06-29 11:36:13 +0100

    cyberPet1

---
+++ animals.js
@@ -0,0 +1,37 @@
+class pet {
+    constructor (name, type, age) {
+    this.name = name;
+    this.type = type;
+    this.age = age;
+    this.health = 100;
+    this.hunger = 100;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am a ${this.type}.`);
+    }
+    
+    drinks() {
+        this.health += 10;
+        console.log('slurp slurp slurp....burp')
+        return this;
+    }
+    eats(){
+        this.health += 5;
+        this.hunger += 10;
+        console.log(`${this.name} has been fed and his health is ${this.health}`);
+        return this;
+    }
+    stats(){
+        return console.log(`My health is ${this.health}, my hunger is ${this.hunger}`);
+    }
+    play(){
+        this.health += 20;
+        this.hunger -= 10;
+        console.log(`You played fetch with ${this.name}, you threw a ball, he came back with human arm.... `)
+    }
+}
+const otis = new pet('Otis', 'French Bulldog', 1);
+otis.greet();
+otis.eats();
+otis.play();
---
+++ game.js
@@ -0,0 +1,31 @@
+const inquirer = require('inquirer');
+const {cthulu} = require('./cthulu')
+const {cannibal} = require('./cannibal')
+
+let myPet;
+
+async function start() {
+    const {typeOfPet} = await inquirer.prompt({
+        type: 'list',
+        name: 'typeOfPet',
+        message:
+        'What type of pet would you like? Please choose from the following:',
+        choices: [
+            {
+                key: 'a',
+                name: 'Dog',
+                value: 'dog',
+            },
+            {
+                key: 'b',
+                name: 'Cthulu',
+                value: 'cthulu',
+            },
+            {
+                key: 'c',
+                name: 'Cannibal',
+                value: 'cannibal',
+            },
+        ]
+    })
+};
